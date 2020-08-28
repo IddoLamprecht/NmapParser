86c2bcaaec97637e9a01f8a725474dec20f8c081
IddoLamprecht <62558105+IddoLamprecht@users.noreply.github.com>
2020-08-28 19:01:37 +0300

    add the comments to the config file
    Signed-off-by: IddoLamprecht <62558105+IddoLamprecht@users.noreply.github.com>

--- config.py
+++ config.py
@@ -2,25 +2,29 @@
 import configparser
 
 config = configparser.ConfigParser()
-
+# all the elements of the file logs (logs that are relevant for all the data logs in the file)
 config["FileElem"] = {
     "scaninfo": ""
 }
+
+# all the elements in the file that have the relevant data that i want in the csv file
 config["LogElem"] = {
     "host": "",
 }
 
+# a dictionary that has all the paths from each attribute that i can get the data for the data logs
 config["host"] = {
     "address": "",
     "port": "ports/",
     "Status": ""
 }
 
-config["scaninfo"]= {
+# a dictionary that has all the paths from each element that i can get the data for the file logs
+config["scaninfo"] = {
     "type": ""
 }
 
-
+# the mapping of what data i want in the rows of the csv file
 config["Extract"] = {
     "address": ["addr"],
     "port": ["portid", "protocol"],
@@ -28,10 +32,11 @@ config["Extract"] = {
     "type": ["type"]
 
 }
+# the file path that im reading from
 config["Directories"] = {
     "Nmap_XML_dir": "/Users/iddolamprecht/Desktop/Normalize/",
 
 }
-
+# create the config file
 with open('ExtractNmapLogs.ini', 'w') as configfile:
     config.write(configfile)
